e0a4a0884247fe9c601952da4492aa86ae80911b
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-10-22 23:52:22 -0300

    Pega primeira APAC

--- funcoesApoio.R
+++ funcoesApoio.R
@@ -28,3 +28,31 @@ convert_mort = function(dataset,colunas){
   }
   return(dataset)
 }
+
+#Cria a base de dados referente às primeiras APACs 
+cria_df_primeira_apac = function(df){
+  hashNumbers = df$AP_CNSPCN
+  hashNumbersUnique = unique(hashNumbers)
+  
+  for(i in 1:length(hashNumbersUnique)){
+    cat(i)
+    cat(", ")
+    id = hashNumbersUnique[i]
+    df_id = df[df$AP_CNSPCN == id,]
+    min_date = min(df_id$AP_CMP)
+    line_to_add = df_id[df_id$AP_CMP == min_date,][1,]
+    line_to_add$MIN_VAL = min(df_id$AP_VL_AP)
+    line_to_add$MAX_VAL = max(df_id$AP_VL_AP)
+    line_to_add$MEAN_VAL = mean(df_id$AP_VL_AP)
+    line_to_add$NUM_APACS = length(df_id$AP_CMP)
+    line_to_add$QTD_LEITOS = mean(df_id$qtd_leitos)
+    line_to_add$QTD_MEDICOS = mean(df_id$qtd_medicos)
+    
+    if(i==1){
+      df_final = line_to_add
+    }else{
+      df_final = rbind(df_final,line_to_add)
+    }
+  }
+  return(df_final)
+}
---
+++ script3.R
@@ -0,0 +1,3 @@
+#Cria a base de dados referente às primeiras APACs 
+apac_linfoma = cria_df_primeira_apac(apac_linfoma)
+write.csv(apac_linfoma,"apac_linfoma_unificado3.csv")
