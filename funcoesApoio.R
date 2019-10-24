103ec1f4fd08fae4eb92168a163ee4b10b15fb43
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-10-23 23:52:38 -0300

    Add files via upload

--- funcoesApoio.R
+++ funcoesApoio.R
@@ -56,3 +56,111 @@ cria_df_primeira_apac = function(df){
   }
   return(df_final)
 }
+
+# Converte nome das colunas para data
+cols_data = function(dataset){
+  colunas = c('Município')
+  for(i in 2:ncol(dataset)){
+    atual = colnames(dataset)[i]
+    year = substr(atual,3,6)
+    month = substr(atual,8,10)
+    nome_correto = paste(year,str_pad(match(month,month.abb),2,pad="0"),"01",sep="")
+    colunas = c(colunas,nome_correto)
+  }
+  colnames(dataset) = colunas
+  dataset$AP_UFMUN = substr(dataset$Município,1,6)
+  return(dataset)
+}
+
+# Cruza tabelas
+cruza = function(apac_linfoma){
+  for(i in 1:nrow(apac_linfoma)){
+    municipio_apac = apac_linfoma[i,'AP_MUNPCN']
+    i_municipio_dataset = grep(municipio_apac,medicos$Município)
+    if(length(i_municipio_dataset)!=0){
+      data_apac = apac_linfoma[i,'AP_CMP']
+      apac_linfoma[i,'QTD_MEDICOS'] = medicos[i_municipio_dataset,data_apac]
+      apac_linfoma[i,'QTD_LEITOS'] = leitos[i_municipio_dataset,data_apac]
+      apac_linfoma[i,'QTD_HESP'] = hesp[i_municipio_dataset,data_apac]
+      apac_linfoma[i,'QTD_HGERAL'] = hgeral[i_municipio_dataset,data_apac]
+      apac_linfoma[i,'QTD_CESP'] = cesp[i_municipio_dataset,data_apac]
+      apac_linfoma[i,'QTD_UBS'] = ubs[i_municipio_dataset,data_apac]
+      apac_linfoma[i,'QTD_UADT'] = uadt[i_municipio_dataset,data_apac]
+    }
+    else{
+        apac_linfoma[i,'QTD_MEDICOS'] = -1
+        apac_linfoma[i,'QTD_LEITOS'] = -1
+        apac_linfoma[i,'QTD_HESP'] = -1
+        apac_linfoma[i,'QTD_HGERAL'] = -1
+        apac_linfoma[i,'QTD_CESP'] = -1
+        apac_linfoma[i,'QTD_UBS'] = -1
+        apac_linfoma[i,'QTD_UADT'] = -1
+    }
+    cat(i)
+    cat(" ")
+  }
+  return(apac_linfoma)
+}
+
+# Retorna UF
+retornaUF = function(dataset){
+  for(i in 1:nrow(dataset)){
+    cod_uf = substr(dataset[i,'AP_MUNPCN'],1,2)
+    if(cod_uf=='11')
+      dataset[i,'UF']='RO'
+    else if(cod_uf=='12')
+      dataset[i,'UF']='AC'
+    else if(cod_uf=='13')
+      dataset[i,'UF']='AM'
+    else if(cod_uf=='14')
+      dataset[i,'UF']='RR'
+    else if(cod_uf=='15')
+      dataset[i,'UF']='PA'
+    else if(cod_uf=='16')
+      dataset[i,'UF']='AP'
+    else if(cod_uf=='17')
+      dataset[i,'UF']='TO'
+    else if(cod_uf=='21')
+      dataset[i,'UF']='MA'
+    else if(cod_uf=='22')
+      dataset[i,'UF']='PI'
+    else if(cod_uf=='23')
+      dataset[i,'UF']='CE'
+    else if(cod_uf=='24')
+      dataset[i,'UF']='RN'
+    else if(cod_uf=='25')
+      dataset[i,'UF']='PB'
+    else if(cod_uf=='26')
+      dataset[i,'UF']='PE'
+    else if(cod_uf=='27')
+      dataset[i,'UF']='AL'
+    else if(cod_uf=='28')
+      dataset[i,'UF']='SE'
+    else if(cod_uf=='29')
+      dataset[i,'UF']='BA'
+    else if(cod_uf=='31')
+      dataset[i,'UF']='MG'
+    else if(cod_uf=='32')
+      dataset[i,'UF']='ES'
+    else if(cod_uf=='33')
+      dataset[i,'UF']='RJ'
+    else if(cod_uf=='35')
+      dataset[i,'UF']='SP'
+    else if(cod_uf=='41')
+      dataset[i,'UF']='PR'
+    else if(cod_uf=='42')
+      dataset[i,'UF']='SC'
+    else if(cod_uf=='43')
+      dataset[i,'UF']='RS'
+    else if(cod_uf=='50')
+      dataset[i,'UF']='MS'
+    else if(cod_uf=='51')
+      dataset[i,'UF']='MT'
+    else if(cod_uf=='52')
+      dataset[i,'UF']='GO'
+    cat(i)
+    cat(" ")
+  }
+  return(dataset)
+}
+
---
+++ script.R
@@ -0,0 +1,126 @@
+setwd("~/Documents/Faculdade/10o_Semestre/BCG Datathon/Banco Datathon/Banco Datathon")
+
+#=======================================================================================
+# CRIA TABELA UNIFICADA
+#=======================================================================================
+
+# Le o Mortalidade Hodgkin
+mort_hodgkin = read.csv("Mortalidade Linfoma de Hodgkin SIM-SUS.csv")
+mort_naohodgkin = read.csv("Mortalidade Linfoma não Hodgkin SIM-SUS.csv")
+
+mort_linfoma = rbind(mort_hodgkin,mort_naohodgkin)
+mort_linfoma = convert_mort(mort_linfoma,c('DTOBITO','DTNASC'))
+
+# Le Radio e Quimio Linfoma
+# Tem que ter salvo os dados como UTF-8 no SUBLIME!
+radio_linfoma = read.csv("Linfomas Radioterapia SIA-SUS.csv", stringsAsFactors = FALSE)
+quimio_linfoma = read.csv("Linfomas Quimioterapia SIA-SUS.csv", stringsAsFactors = FALSE)
+
+radio_linfoma_reduz = radio_linfoma[c('AP_MVM','AP_CONDIC','AP_GESTAO','AP_CODUNI','AP_AUTORIZ','AP_CMP',
+                                      'AP_PRIPAL','AP_VL_AP','AP_UFMUN','AP_TPUPS','AP_TIPPRE','AP_CNPJCPF',
+                                      'AP_CNPJMNT','AP_CNSPCN','AP_COIDADE','AP_NUIDADE','AP_SEXO','AP_RACACOR',
+                                      'AP_MUNPCN','AP_UFNACIO','AP_CEPPCN','AP_UFDIF','AP_MNDIF','AP_DTINIC',
+                                      'AP_DTFIM','AP_TPATEN','AP_TPAPAC','AP_MOTSAI','AP_OBITO','AP_APACANT',
+                                      'AP_UNISOL','AP_DTSOLIC','AP_DTAUT','AP_CIDCAS','AP_CIDPRI','AP_CIDSEC',
+                                      'AR_ESTADI','AR_DTIDEN','AR_TRANTE','AR_DTINTR')]
+colnames(radio_linfoma_reduz) = c('AP_MVM','AP_CONDIC','AP_GESTAO','AP_CODUNI','AP_AUTORIZ','AP_CMP',
+                                  'AP_PRIPAL','AP_VL_AP','AP_UFMUN','AP_TPUPS','AP_TIPPRE','AP_CNPJCPF',
+                                  'AP_CNPJMNT','AP_CNSPCN','AP_COIDADE','AP_NUIDADE','AP_SEXO','AP_RACACOR',
+                                  'AP_MUNPCN','AP_UFNACIO','AP_CEPPCN','AP_UFDIF','AP_MNDIF','AP_DTINIC',
+                                  'AP_DTFIM','AP_TPATEN','AP_TPAPAC','AP_MOTSAI','AP_OBITO','AP_APACANT',
+                                  'AP_UNISOL','AP_DTSOLIC','AP_DTAUT','AP_CIDCAS','AP_CIDPRI','AP_CIDSEC',
+                                  'AP_ESTADI','AP_DTIDEN','AP_TRANTE','AP_DTINTR')
+
+quimio_linfoma_reduz = quimio_linfoma[c('AP_MVM','AP_CONDIC','AP_GESTAO','AP_CODUNI','AP_AUTORIZ','AP_CMP',
+                                        'AP_PRIPAL','AP_VL_AP','AP_UFMUN','AP_TPUPS','AP_TIPPRE','AP_CNPJCPF',
+                                        'AP_CNPJMNT','AP_CNSPCN','AP_COIDADE','AP_NUIDADE','AP_SEXO','AP_RACACOR',
+                                        'AP_MUNPCN','AP_UFNACIO','AP_CEPPCN','AP_UFDIF','AP_MNDIF','AP_DTINIC',
+                                        'AP_DTFIM','AP_TPATEN','AP_TPAPAC','AP_MOTSAI','AP_OBITO','AP_APACANT',
+                                        'AP_UNISOL','AP_DTSOLIC','AP_DTAUT','AP_CIDCAS','AP_CIDPRI','AP_CIDSEC',
+                                        'AQ_ESTADI','AQ_DTIDEN','AQ_TRANTE','AQ_DTINTR')]
+colnames(quimio_linfoma_reduz) = c('AP_MVM','AP_CONDIC','AP_GESTAO','AP_CODUNI','AP_AUTORIZ','AP_CMP',
+                                   'AP_PRIPAL','AP_VL_AP','AP_UFMUN','AP_TPUPS','AP_TIPPRE','AP_CNPJCPF',
+                                   'AP_CNPJMNT','AP_CNSPCN','AP_COIDADE','AP_NUIDADE','AP_SEXO','AP_RACACOR',
+                                   'AP_MUNPCN','AP_UFNACIO','AP_CEPPCN','AP_UFDIF','AP_MNDIF','AP_DTINIC',
+                                   'AP_DTFIM','AP_TPATEN','AP_TPAPAC','AP_MOTSAI','AP_OBITO','AP_APACANT',
+                                   'AP_UNISOL','AP_DTSOLIC','AP_DTAUT','AP_CIDCAS','AP_CIDPRI','AP_CIDSEC',
+                                   'AP_ESTADI','AP_DTIDEN','AP_TRANTE','AP_DTINTR')
+
+apac_linfoma = rbind(radio_linfoma_reduz,quimio_linfoma_reduz)
+apac_linfoma = convert_apac(apac_linfoma,c('AP_CMP'))
+
+# Limpa
+rm(mort_hodgkin)
+rm(mort_naohodgkin)
+rm(quimio_linfoma_reduz)
+rm(quimio_linfoma)
+rm(radio_linfoma_reduz)
+rm(radio_linfoma)
+
+#Escreve CSVs
+write.csv(apac_linfoma,"apac_linfoma_unificado.csv")
+write.csv(mort_linfoma,"mort_linfoma_unificado.csv")
+
+#=======================================================================================
+# CRUZA COM OUTRAS TABELAS
+#=======================================================================================
+
+library("stringr")
+apac_linfoma$AP_CMP = as.character(apac_linfoma$AP_CMP)
+
+# Le base
+# Tem que ter excluido o comecinho
+# Tem que ter colocado os meses em ingles
+# Tem que trocar ; por ,
+medicos = read.csv("RH- Médicos.csv",stringsAsFactors = FALSE,quote="")
+medicos = cols_data(medicos)
+medicos = medicos[order(medicos$Município),]
+
+leitos = read.csv("RF- Leitos de Internação.csv",stringsAsFactors = FALSE,quote="")
+leitos = cols_data(leitos)
+leitos = leitos[order(leitos$Município),]
+
+hesp = read.csv("Estabelecimentos- Hospital Especializado.csv",stringsAsFactors = FALSE,quote="")
+hesp = cols_data(hesp)
+hesp = hesp[order(hesp$Município),]
+
+hgeral = read.csv("Estabelecimentos- Hospital Geral.csv",stringsAsFactors = FALSE,quote="")
+hgeral = cols_data(hgeral)
+hgeral = hgeral[order(hgeral$Município),]
+
+cesp = read.csv("Estabelecimentos- Clínicas-Ambulatórios Especializados.csv",stringsAsFactors = FALSE,quote="")
+cesp = cols_data(cesp)
+cesp = cesp[order(cesp$Município),]
+
+ubs = read.csv("Estabelecimentos- Unidade Básica de Saúde.csv",stringsAsFactors = FALSE,quote="")
+ubs = cols_data(ubs)
+ubs = ubs[order(ubs$Município),]
+
+uadt = read.csv("Estabelecimentos- Unidade de Serviço de Apoio ao Diagnose e Terapia.csv",stringsAsFactors = FALSE,quote="")
+uadt = cols_data(uadt)
+uadt = uadt[order(uadt$Município),]
+
+# Combina com a base de apac_linfoma
+apac_linfoma = cruza(apac_linfoma)
+
+apac_linfoma$QTD_MEDICOS[apac_linfoma$QTD_MEDICOS=='-'] = '0'
+apac_linfoma$QTD_LEITOS[apac_linfoma$QTD_LEITOS=='-'] = '0'
+apac_linfoma$QTD_HESP[apac_linfoma$QTD_HESP=='-'] = '0'
+apac_linfoma$QTD_HGERAL[apac_linfoma$QTD_HGERAL=='-'] = '0'
+apac_linfoma$QTD_CESP[apac_linfoma$QTD_CESP=='-'] = '0'
+apac_linfoma$QTD_UBS[apac_linfoma$QTD_UBS=='-'] = '0'
+apac_linfoma$QTD_UADT[apac_linfoma$QTD_UADT=='-'] = '0'
+apac_linfoma$QTD_MEDICOS[apac_linfoma$QTD_MEDICOS=='-1'] = '0'
+apac_linfoma$QTD_LEITOS[apac_linfoma$QTD_LEITOS=='-1'] = '0'
+apac_linfoma$QTD_HESP[apac_linfoma$QTD_HESP=='-1'] = '0'
+apac_linfoma$QTD_HGERAL[apac_linfoma$QTD_HGERAL=='-1'] = '0'
+apac_linfoma$QTD_CESP[apac_linfoma$QTD_CESP=='-1'] = '0'
+apac_linfoma$QTD_UBS[apac_linfoma$QTD_UBS=='-1'] = '0'
+apac_linfoma$QTD_UADT[apac_linfoma$QTD_UADT=='-1'] = '0'
+
+# Pega UF
+apac_linfoma = retornaUF(apac_linfoma)
+apac_linfoma = apac_linfoma[,2:ncol(apac_linfoma)]
+
+#Escreve CSVs 
+write.csv(apac_linfoma,"apac_linfoma_unificado2.csv")
